b5e6eb66ece18a6c587093cfd6fb3e03aeb59523
yushulx <lingxiao1002@gmail.com>
2014-08-29 16:31:10 +0800

    update

--- IPCamera/gen/com/yushulx/ipcamera/R.java
+++ IPCamera/gen/com/yushulx/ipcamera/R.java
@@ -24,8 +24,10 @@ public final class R {
         public static final int ic_launcher=0x7f020000;
     }
     public static final class id {
-        public static final int action_send=0x7f080001;
-        public static final int action_settings=0x7f080000;
+        public static final int action_send=0x7f080003;
+        public static final int action_settings=0x7f080002;
+        public static final int button_capture=0x7f080001;
+        public static final int camera_preview=0x7f080000;
     }
     public static final class layout {
         public static final int main=0x7f030000;
--- IPCamera/src/com/yushulx/ipcamera/IPCamera.java
+++ IPCamera/src/com/yushulx/ipcamera/IPCamera.java
@@ -1,16 +1,40 @@
 package com.yushulx.ipcamera;
 
-import android.os.Bundle;
 import android.app.Activity;
+import android.hardware.Camera;
+import android.hardware.Camera.PreviewCallback;
+import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.FrameLayout;
+import android.widget.Toast;
 
 public class IPCamera extends Activity {
+    private CameraPreview mPreview;
+    private CameraManager mCameraManager;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.main);
+		
+		Button captureButton = (Button) findViewById(R.id.button_capture);
+		captureButton.setOnClickListener(
+		    new View.OnClickListener() {
+		        @Override
+		        public void onClick(View v) {
+		            // get an image from the camera
+		          
+		        }
+		    }
+		);
+		mCameraManager = new CameraManager(this);
+        // Create our Preview view and set it as the content of our activity.
+        mPreview = new CameraPreview(this, mCameraManager.getCamera());
+        FrameLayout preview = (FrameLayout) findViewById(R.id.camera_preview);
+        preview.addView(mPreview);
 	}
 
 	@Override
@@ -31,5 +55,19 @@ public class IPCamera extends Activity {
 		}
 		
 		return super.onOptionsItemSelected(item);
+	}
+	
+	@Override
+    protected void onPause() {
+        super.onPause();
+        mCameraManager.onPause();              // release the camera immediately on pause event
+    }
+	
+	@Override
+	protected void onResume() {
+		// TODO Auto-generated method stub
+		super.onResume();
+		mCameraManager.onResume();
+		mPreview.setCamera(mCameraManager.getCamera());
 	}
 }
